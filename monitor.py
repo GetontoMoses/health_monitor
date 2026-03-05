e2a240df6670b8e5e26012498b7eba105dc8abf6
Getonto3 <getontomoses18@gmail.com>
2026-03-06 01:37:04 +0300

    the script

---
+++ monitor.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+def check_url(url):
+    try:
+        response = requests.get(url,timeout=5)
+        
+        if response.status_code == 200:
+            print(f"{url} is up and running")
+        else:
+            print(f"{url}creturned {response.status_code}")
+    except Exception as e:
+        print(f"{url} is Down")
+        
+    
+    def monitor():
+        with open("urls.txt") as f:
+            urls = f.read().splitlines()
+        
+        for url in urls:
+            check_url(url)
+    
+    if __name__ == "__main__":
+        monitor
